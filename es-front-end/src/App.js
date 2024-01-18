21fcfec886b7428efda9b3536c1e1cc207a1cb4e
Aseem <aseem@Aseems-MBP.lan>
2024-01-18 17:52:52 -0500

    initial commit

---
+++ es-front-end/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/main/java/ec/controller/HomeController.java
@@ -0,0 +1,18 @@
+package ec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+}
+
+
