984dfe2670b4155bb064043f8cda9dd75d473b6a
Aseem <aseem@Aseems-MBP.lan>
2024-01-24 22:19:57 -0500

    delete all files

--- es-front-end/src/components/AboutUs.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const AboutUs = () => <h1>About Page</h1>;
-
-export default AboutUs;
--- es-front-end/src/components/ContactUs.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ContactUs = () => <h1>Contact Us</h1>;
-
-export default ContactUs;
